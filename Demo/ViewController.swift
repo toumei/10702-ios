be22d5df0b6a3b5aa3a48a2b3e20a35cf6d7cd98
toumei <ricknbmnwo@gmail.com>
2019-06-21 18:01:53 +0800

    update

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -10,15 +10,34 @@ import UIKit
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
     
-    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var spinButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
     
-    let fullScreenSize = UIScreen.main.bounds.size
-    var imageArray = [#imageLiteral(resourceName: "dimond"),#imageLiteral(resourceName: "crown"),#imageLiteral(resourceName: "bar"),#imageLiteral(resourceName: "seven"),#imageLiteral(resourceName: "cherry"),#imageLiteral(resourceName: "lemon")]
-    
+    var imageArray: [UIImage] = []
+    var pickerCol1: [Int] = []
+    var pickerCol2: [Int] = []
+    var pickerCol3: [Int] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        
+        imageArray = [#imageLiteral(resourceName: "dimond"),#imageLiteral(resourceName: "crown"),#imageLiteral(resourceName: "bar"),#imageLiteral(resourceName: "seven"),#imageLiteral(resourceName: "cherry"),#imageLiteral(resourceName: "lemon")]
+        resultLabel.text = " "
+        
+        for _ in 0...99 {
+            pickerCol1.append(Int.random(in: 0...imageArray.count-1))
+            pickerCol2.append(Int.random(in: 0...imageArray.count-1))
+            pickerCol3.append(Int.random(in: 0...imageArray.count-1))
+        }
+        
+        for i in 0...2 {
+            picker.selectRow(50, inComponent: i, animated: false)
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -26,14 +45,24 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func buttonClicked(sender: AnyObject) {
+    
+    @IBAction func buttonClicked(_ sender: Any) {
         for i in 0...2 {
-            pickerView.selectRow(Int.random(in: 0...100), inComponent: i, animated: true)
+            picker.selectRow(Int.random(in: 1...98), inComponent: i, animated: true)
+        }
+        let col1 = pickerCol1[picker.selectedRow(inComponent: 0)]
+        let col2 = pickerCol2[picker.selectedRow(inComponent: 1)]
+        let col3 = pickerCol3[picker.selectedRow(inComponent: 2)]
+        
+        
+        if (col1 == col2) && (col2 == col3) {
+            resultLabel.text = "Bingo!!"
+        } else {
+            resultLabel.text = " "
         }
     }
     
-    // pickerView setting
-    
+    // UIPickerViewDataSource
     // rowNum
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return 100
@@ -44,6 +73,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         return 3
     }
     
+    // // UIPickerViewDelegate
     // width
     func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
         return imageArray[component].size.width + 35
@@ -56,10 +86,20 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     // content
     func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
-        for i in 0...2 {
-            pickerView.selectRow(50, inComponent: i, animated: true)
+        var img: UIImage = UIImage.init()
+        switch component {
+        case 0:
+            img = imageArray[pickerCol1[row]]
+        case 1:
+            img = imageArray[pickerCol2[row]]
+        case 2:
+            img = imageArray[pickerCol3[row]]
+        default:
+            print("Error")
         }
-        return UIImageView(image: imageArray.randomElement())
+        
+        return UIImageView(image: img)
     }
+    
 }
 
